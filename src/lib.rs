0d146355a0eeab3ad03c170ba03ac798a17a47ab
petru <smart__petea@mail.ru>
2022-06-08 14:22:54 +0300

    first steps

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,53 @@
+const CHAR_64: [char; 64] = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '_'];
+
 pub fn encode<T: AsRef<[u8]>>(input: T) -> String {
     let mut result = String::new();
     let input = input.as_ref();
 
-    for i in (0..(input.len()-3)).step_by(3) {
-        let i1 = input[i] >> 2;
-        let i2 = ((input[i] & 0b00000011) << 4) | (input[i+1] >> 4);
-        let i3 = ((input[i+1] & 0b00001111) << 2) | (input[i+2] >> 6);
-        let i4 = input[i+2] & 0b00111111;
+    if input.len() == 0 {
+        return result;
+    }
 
-        println!("[b1 b2 b3] = [{} {} {}] [i1 i2 i3 i4]=[{} {} {} {}]", input[i], input[i+1], input[i+2], i1, i2, i3, i4);
+    if input.len() >= 3 {
+        for i in (0..=(input.len()-2)).step_by(3) {
+            let i1 = input[i] >> 2;
+            let i2 = ((input[i] & 0b00000011) << 4) | (input[i+1] >> 4);
+            let i3 = ((input[i+1] & 0b00001111) << 2) | (input[i+2] >> 6);
+            let i4 = input[i+2] & 0b00111111;
+
+            result.push(CHAR_64[i1 as usize]);
+            result.push(CHAR_64[i2 as usize]);
+            result.push(CHAR_64[i3 as usize]);
+            result.push(CHAR_64[i4 as usize]);
+        }
+    }
+
+    match input.len() % 3 {
+        1 => {
+            println!("input.len={}", input.len());
+            let i = input.len() - 1;
+            let i1 = input[i] >> 2;
+            let i2 = (input[i] & 0b00000011) << 4;
+
+            result.push(CHAR_64[i1 as usize]);
+            result.push(CHAR_64[i2 as usize]);
+
+            result.push('=');
+            result.push('=');
+        }
+        2 => {
+            let i = input.len() - 2;
+            let i1 = input[i] >> 2;
+            let i2 = ((input[i] & 0b00000011) << 4) | (input[i+1] >> 4);
+            let i3 = (input[i+1] & 0b00001111) << 2;
+
+            result.push(CHAR_64[i1 as usize]);
+            result.push(CHAR_64[i2 as usize]);
+            result.push(CHAR_64[i3 as usize]);
+
+            result.push('=');
+        }
+        _ => ()
     }
 
     result
@@ -16,9 +55,28 @@ pub fn encode<T: AsRef<[u8]>>(input: T) -> String {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn encode_test() {
+        let input = "";
+        let expected_output = "".to_string();
+        let real_output = encode(input);
+        assert_eq!(real_output, expected_output);
+
+        let input = "f";
+        let expected_output = "Zg==".to_string();
+        let real_output = encode(input);
+        assert_eq!(real_output, expected_output);
+
+        let input = "fo";
+        let expected_output = "Zm8=".to_string();
+        let real_output = encode(input);
+        assert_eq!(real_output, expected_output);
+
+        let input = "foo";
+        let expected_output = "Zm9v".to_string();
+        let real_output = encode(input);
+        assert_eq!(real_output, expected_output);
     }
 }

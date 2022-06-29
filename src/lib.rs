397c2a8c922cd79243df4e5ffab3b919387d84b3
petru <smart__petea@mail.ru>
2022-06-29 17:19:07 +0300

    29/VI.2022

--- src/lib.rs
+++ src/lib.rs
@@ -39,6 +39,7 @@ const CRYPT_U8: [u8; 64] = [ '.' as u8, '/' as u8, '0' as u8, '1' as u8, '2' as 
 const BCRYPT_U8: [u8; 64] = [ '.' as u8, '/' as u8, 'A' as u8, 'B' as u8, 'C' as u8, 'D' as u8, 'E' as u8, 'F' as u8, 'G' as u8, 'H' as u8, 'I' as u8, 'J' as u8, 'K' as u8, 'L' as u8, 'M' as u8, 'N' as u8, 'O' as u8, 'P' as u8, 'Q' as u8, 'R' as u8, 'S' as u8, 'T' as u8, 'U' as u8, 'V' as u8, 'W' as u8, 'X' as u8, 'Y' as u8, 'Z' as u8, 'a' as u8, 'b' as u8, 'c' as u8, 'd' as u8, 'e' as u8, 'f' as u8, 'g' as u8, 'h' as u8, 'i' as u8, 'j' as u8, 'k' as u8, 'l' as u8, 'm' as u8, 'n' as u8, 'o' as u8, 'p' as u8, 'q' as u8, 'r' as u8, 's' as u8, 't' as u8, 'u' as u8, 'v' as u8, 'w' as u8, 'x' as u8, 'y' as u8, 'z' as u8, '0' as u8, '1' as u8, '2' as u8, '3' as u8, '4' as u8, '5' as u8, '6' as u8, '7' as u8, '8' as u8, '9' as u8];
 const IMAP_MUTF7_U8: [u8; 64] = [ 'A' as u8, 'B' as u8, 'C' as u8, 'D' as u8, 'E' as u8, 'F' as u8, 'G' as u8, 'H' as u8, 'I' as u8, 'J' as u8, 'K' as u8, 'L' as u8, 'M' as u8, 'N' as u8, 'O' as u8, 'P' as u8, 'Q' as u8, 'R' as u8, 'S' as u8, 'T' as u8, 'U' as u8, 'V' as u8, 'W' as u8, 'X' as u8, 'Y' as u8, 'Z' as u8, 'a' as u8, 'b' as u8, 'c' as u8, 'd' as u8, 'e' as u8, 'f' as u8, 'g' as u8, 'h' as u8, 'i' as u8, 'j' as u8, 'k' as u8, 'l' as u8, 'm' as u8, 'n' as u8, 'o' as u8, 'p' as u8, 'q' as u8, 'r' as u8, 's' as u8, 't' as u8, 'u' as u8, 'v' as u8, 'w' as u8, 'x' as u8, 'y' as u8, 'z' as u8, '0' as u8, '1' as u8, '2' as u8, '3' as u8, '4' as u8, '5' as u8, '6' as u8, '7' as u8, '8' as u8, '9' as u8, '+' as u8, ',' as u8];
 const BIN_HEX_U8: [u8; 64] = ['!' as u8, '"' as u8, '#' as u8, '$' as u8, '%' as u8, '&' as u8, '\'' as u8, '(' as u8, ')' as u8, '*' as u8, '+' as u8, ',' as u8, '-' as u8, '0' as u8, '1' as u8, '2' as u8, '3' as u8, '4' as u8, '5' as u8, '6' as u8, '8' as u8, '9' as u8, '@' as u8, 'A' as u8, 'B' as u8, 'C' as u8, 'D' as u8, 'E' as u8, 'F' as u8, 'G' as u8, 'H' as u8, 'I' as u8, 'J' as u8, 'K' as u8, 'L' as u8, 'M' as u8, 'N' as u8, 'P' as u8, 'Q' as u8, 'R' as u8, 'S' as u8, 'T' as u8, 'U' as u8, 'V' as u8, 'X' as u8, 'Y' as u8, 'Z' as u8, '[' as u8, '`' as u8, 'a' as u8, 'b' as u8, 'c' as u8, 'd' as u8, 'e' as u8, 'f' as u8, 'h' as u8, 'i' as u8, 'j' as u8, 'k' as u8, 'l' as u8, 'm' as u8, 'p' as u8, 'q' as u8, 'r' as u8];
+const EQUAL_U8: [u8; 1] = ['=' as u8];
 
 const STANDARD: Config = Config::new(CharacterSet::Standard, true);
 const STANDARD_NO_PAD: Config = Config::new(CharacterSet::Standard, false);
@@ -48,12 +49,13 @@ const URL_SAFE_NO_PAD: Config = Config::new(CharacterSet::UrlSafe, false);
 pub fn encode_config_slice<T: AsRef<[u8]>>(
     input: T,
     config: Config,
-    buf: &mut [u8]
+    mut writer: &mut [u8]
 ) -> usize {
     let input = input.as_ref();
+    let mut written = 0usize;
 
     if input.len() == 0 {
-        return 0;
+        return written;
     }
 
     let u8s = match config.char_set {
@@ -73,43 +75,88 @@ pub fn encode_config_slice<T: AsRef<[u8]>>(
             let i3 = (((input[i+1] & 0b00001111) << 2) | (input[i+2] >> 6)) as usize;
             let i4 = (input[i+2] & 0b00111111) as usize;
 
-            buf.write(&u8s[i1..i1+1]);
-            buf.write(&u8s[i2..i2+1]);
-            buf.write(&u8s[i3..i3+1]);
-            buf.write(&u8s[i4..i4+1]);
+            match writer.write(&u8s[i1..i1+1]) {
+                Ok(w) => written += w,
+                Err(_err) => return written
+            };
+
+            match writer.write(&u8s[i2..i2+1]) {
+                Ok(w) => written += w,
+                Err(_err) => return written
+            }
+
+            match writer.write(&u8s[i3..i3+1]) {
+                Ok(w) => written += w,
+                Err(_err) => return written
+            }
+
+            match writer.write(&u8s[i4..i4+1]) {
+                Ok(w) => written += w,
+                Err(_err) => return written
+            }
         }
     }
 
     match input.len() % 3 {
         1 => {
             let i = input.len() - 1;
-            let i1 = input[i] >> 2;
-            let i2 = (input[i] & 0b00000011) << 4;
+            let i1 = (input[i] >> 2) as usize;
+            let i2 = ((input[i] & 0b00000011) << 4) as usize;
 
-            buf.push(chars[i1 as usize]);
-            buf.push(chars[i2 as usize]);
+            match writer.write(&u8s[i1..i1+1]) {
+                Ok(w) => written += w,
+                Err(_err) => return written
+            };
+
+            match writer.write(&u8s[i2..i2+2]) {
+                Ok(w) => written += w,
+                Err(_err) => return written
+            };
 
             if config.pad {
-                buf.push('=');
-                buf.push('=');
+                match writer.write(&EQUAL_U8[..]) {
+                    Ok(w) => written += w,
+                    Err(_err) => return written
+                };
+
+                match writer.write(&EQUAL_U8[..]) {
+                    Ok(w) => written += w,
+                    Err(_err) => return written
+                };
             }
         }
         2 => {
             let i = input.len() - 2;
-            let i1 = input[i] >> 2;
-            let i2 = ((input[i] & 0b00000011) << 4) | (input[i+1] >> 4);
-            let i3 = (input[i+1] & 0b00001111) << 2;
+            let i1 = (input[i] >> 2) as usize;
+            let i2 = (((input[i] & 0b00000011) << 4) | (input[i+1] >> 4)) as usize;
+            let i3 = ((input[i+1] & 0b00001111) << 2) as usize;
 
-            buf.push(chars[i1 as usize]);
-            buf.push(chars[i2 as usize]);
-            buf.push(chars[i3 as usize]);
+            match writer.write(&u8s[i1..i1+1]) {
+                Ok(w) => written += w,
+                Err(_err) => return written
+            };
+
+            match writer.write(&u8s[i2..i2+1]) {
+                Ok(w) => written += w,
+                Err(_err) => return written
+            };
+
+            match writer.write(&u8s[i3..i3+1]) {
+                Ok(w) => written += w,
+                Err(_err) => return written
+            }
 
             if config.pad {
-                buf.push('=');
+                match writer.write(&EQUAL_U8[..]) {
+                    Ok(w) => written += w,
+                    Err(_err) => return written
+                };
             }
         }
         _ => ()
-    }
+    };
+
+    written
 }
 
 pub fn encode_config_buf<T: AsRef<[u8]>>(
